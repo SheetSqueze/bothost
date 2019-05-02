65b9bc28ae3de8cd493c413656185aaf89d60acc
Nox <40596839+SheetSqueze@users.noreply.github.com>
2019-05-02 16:17:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'herokutest') {
 
-       message.reply('pong');
+       message.reply('credistz to heroku site');
 
        }
 
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NTcyODAxNzY4NDA1MzM2MDk2.XMoj_g.9GQeZ6c4SMVulG9cVmxlQv5yRKk);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
