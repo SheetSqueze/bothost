3cc848204a60d6caa60cae03b9608ca366bde7b6
Nox <40596839+SheetSqueze@users.noreply.github.com>
2019-09-01 09:20:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,12 @@
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
- 
+const Discord = require("discord.js")
+const fs = require("fs")
+const client = new Discord.Client()
 
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+   client.user.setPresence({
+        game: {
+            name: "cheap origin with games accounts",
+            type: "Streaming"
+        }
+   })
 });
-
- 
-
-client.on('message', message => {
-
-    if (message.content === 'herokutest') {
-
-       message.reply('credistz to heroku site');
-
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
