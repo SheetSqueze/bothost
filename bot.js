716330f01e976734878e20a850de5530aeca4c10
Nox <40596839+SheetSqueze@users.noreply.github.com>
2019-09-01 10:54:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,22 @@
 const Discord = require("discord.js")
 const fs = require("fs")
 const client = new Discord.Client()
+const config = require("./config.json")
 
 client.on('ready', () => {
    client.user.setPresence({
         game: {
             name: config.status,
-            type: "STREAMING"
+            type: "WATCHING"
         }
    })
+
+   console.log(`You can invite this bot to your server with this link: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+   console.log(`You can invite this bot to your server with this link: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+   console.log(`You can invite this bot to your server with this link: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+   console.log(`You can invite this bot to your server with this link: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+   console.log(`You can invite this bot to your server with this link: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+
 });
 
-client.login(config.token);
+client.login(config.token)
